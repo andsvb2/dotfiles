0d4b6d5042508456cb64d4087bc02da38df3b4c5
Anderson S. Vieira <svieira.anderson@gmail.com>
2020-07-03 11:00:32 -0300

    adiciona ~/.doom.d ao stow

---
+++ doom-emacs/.doom.d/config.el
@@ -0,0 +1,101 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Anderson S. Vieira"
+      user-mail-address "svieira.anderson@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Dropbox/Documentos/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(setq-hook! 'TeX-mode-hook +spellcheck-immediately nil) ;; stop doom from immediately running a spell check on every tex mode file
+
+(add-hook! 'TeX-mode-hook #'(writeroom-mode abbrev-mode hl-todo-mode))
+(setq +latex-viewers '(evince))
+
+(after! writeroom-mode
+  (add-hook 'writeroom-global-effects 'writeroom-set-fullscreen))
+
+(setq ispell-dictionary "pt_BR")
+
+ (autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
+ (setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode))
+                               auto-mode-alist))
+
+(setq! ;; bibtex-autokey-expand-strings t
+       bibtex-autokey-names-stretch 1
+       bibtex-autokey-name-case-convert-function (quote capitalize)
+       bibtex-autokey-additional-names "EtAl"
+       bibtex-autokey-year-length 4
+       bibtex-autokey-titleword-ignore (quote ("O" "Da" "Do" "Dos"))
+       bibtex-autokey-titlewords 3
+       bibtex-autokey-titleword-length (quote infty)
+       bibtex-autokey-titleword-case-convert-function (quote capitalize)
+       bibtex-autokey-titleword-separator ""
+       bibtex-autokey-name-year-separator ":"
+       ;; bibtex-autokey-year-title-separator ":"
+       )
+
+(remove-hook 'org-mode-hook #'org-superstar-mode)
+
+;;; :ui deft
+(setq deft-recursive t
+      deft-use-filter-string-for-filename t
+      deft-default-extension "org"
+      deft-directory (concat org-directory "roam/"))
+
+;;; :lang org
+(setq org-roam-directory (concat org-directory "roam/")
+      org-roam-index-file "index.org"
+      ;; org-journal-encrypt-journal t
+      org-journal-date-prefix "#+TITLE: "
+      org-journal-file-format "%Y-%m-%d.org"
+      org-journal-dir (concat org-directory "roam/")
+      org-journal-date-format "%A, %d %B %Y"
+      org-journal-enable-agenda-integration t
+      org-noter-notes-search-path '("~/Dropbox/Documentos/org/roam/")
+      ;; org-ellipsis " ▼ "
+      org-superstar-headline-bullets-list '("#"))
---
+++ doom-emacs/.doom.d/modules/tools/ebib/config.el
@@ -0,0 +1,81 @@
+;;; tools/ebib/config.el -*- lexical-binding: t; -*-
+
+;; Adaptado do .spacemacs
+
+(after! ebib
+  (setq ebib-bibtex-dialect 'biblatex
+        ebib-bib-search-dirs '("~/LaTeX/and-tese/bib/"
+                               "~/LaTeX/biblatex")
+        ebib-preload-bib-files '("~/LaTeX/and-tese/bib/bibliografia-tese.bib"
+                                 "~/LaTeX/and-tese/bib/zotero-sempre-atualizado.bib")
+        ))
+
+(map! ("C-c b" #'ebib)                   ; Abre ebib do menu global
+      (:map TeX-mode-map
+            ;; :localleader
+            :desc "Cita usando Ebib" "C-c l b" #'ebib-insert-citation))
+
+(setq ebib-extra-fields '((BibTeX "crossref" "annote" "abstract" "keywords" "file" "timestamp" "url" "doi")
+                            (biblatex "crossref" "annotation" "abstract" "keywords" "file" "timestamp" "url" "doi"))
+        ebib-use-timestamp 't
+        ebib-timestamp-format (format-time-string "%Y-%m-%d %H:%M:%S %z")
+        ebib-file-associations '(("pdf" . "evince") ("ps" . "gv"))
+        ebib-citation-commands '((any
+                                   (("cite"        "\\cite%<[%A]%>%<[%A]%>{%(%K%,)}")
+                                    ("text"        "\\textcite%<[%A]%>%<[%A]%>{%(%K%,)}")
+                                    ("cites"       "\\cites%(%<[%A]%>[%A]{%K}%)")
+                                    ("textcites"   "\\textcites%(%<[%A]%>[%A]{%K}%)")
+                                    ("author"      "\\citeauthor%<[%A]%>{%(%K%,)}")
+                                    ("year"        "\\citeyear%<[%A]%>[%A]{%K}")
+                                    ("footcite"    "\\footcite%<[%A]%>[%A]{%K}")
+                                    ("title"       "\\citetitle%<[%A]%>[%A]{%K}")
+                                    ("apud"        "\\apud%(%<[%A]%>[%A]{%K}%)")
+                                    ("textapud"    "\\textapud%(%<[%A]%>[%A]{%K}%)")
+                                    ;; ("paren"       "\\parencite%<[%A]%>[%A]{%(%K%,)}")
+                                    ;; ("possauthor"  "\\possciteauthor%<[%A]%>{%K}")
+                                    ;; ("posstext"    "\\posstextcite%<[%A]%>{%K}")
+                                    ;; ("posscite"    "\\posscite%<[%A]%>{%K}")
+                                    ))
+                                  ;; (org-mode
+                                   ;; (("ebib"        "[[ebib:%K][%D]]")))
+                                  ;; (markdown-mode
+                                   ;; (("text"        "@%K%< [%A]%>")
+                                    ;; ("paren"       "[%(%<%A %>@%K%<, %A%>%; )]")
+                                    ;; ("year"        "[-@%K%< %A%>]")))
+                                  ))
+
+;; Cópia do arquivo original em <https://github.com/patrl/.doom.d/blob/master/modules/tools/ebib/config.el>
+;;
+;;;;; tools/ebib/config.el -*- lexical-binding: t; -*-
+
+;; (after! ebib
+;;   (setq ebib-bibtex-dialect 'biblatex
+;;         ebib-preload-bib-files '("/home/patrl/repos/bibliography/elliott_mybib.bib")
+;;         ebib-index-mode-map (make-sparse-keymap)
+;;         ebib-entry-mode-map (make-sparse-keymap))
+;; (map! (:map (ebib-index-mode-map)
+;;           :n "k" #'ebib-prev-entry
+;;           :n "j" #'ebib-next-entry
+;;           :n "l" #'ebib-next-database
+;;           :n "h" #'ebib-prev-database
+;;           :n "gg" #'ebib-goto-first-entry
+;;           :n "G" #'ebib-goto-last-entry
+;;           :n "q" #'ebib-quit
+;;           :n "/" #'ebib-search
+;;           :n "n" #'ebib-search-next
+;;           :n "x" #'ebib-delete-entry
+
+;;           :localleader
+;;           :n "l" #'ebib-show-log
+;;           :n "a" #'ebib-show-annotation
+;;           :n "n" #'ebib-open-note
+;;           )
+
+;;         (:map (ebib-entry-mode-map)
+;;           :n "k" #'ebib-prev-field
+;;           :n "j" #'ebib-next-field
+;;           :n "RET" #'ebib-edit-field
+;;           :n "gg" #'ebib-goto-first-field
+;;           :n "G" #'ebib-goto-last-field
+;;           :n "q" #'ebib-quit-entry-buffer
+;;           :n "y" #'ebib-yank-field-contents)))
