2d5d983d14e8ea11b4496b3fdb7de2351166ce2c
Anderson S. Vieira <svieira.anderson@gmail.com>
2020-07-04 23:15:25 -0300

    incorpora correções

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -36,10 +36,11 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
+;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
